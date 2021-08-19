763e3e64aae5d3894117706687b7b2112e5ba456
William Menghi Hirano <whirano4@gmail.com>
2021-08-19 12:22:35 -0300

    Update

--- ObstacleAvoidingCar/ObstacleAvoidingCar.ino
+++ ObstacleAvoidingCar/ObstacleAvoidingCar.ino
@@ -8,61 +8,71 @@
           Equipe William
 
 **********************************************************************/
-const int botao = 8;
-const int led_verde = 6;
-const int led_vermelho = 7;
+#define botao  8
+#define led_verde 6
+#define led_vermelho 7
+#define motor1control 3
+#define motor1A 2
+#define motor1B 4
+#define motor2control 5
+#define motor2A 12
+#define motor2B 13
+
 int sensor;
-int motor_1A = 2;
-int motor_1B = 3;
-int motor_2A = 4;
-int motor_2B = 5;
 int tempo_inicial;
 int tempo_parcial;
 int tempo_final;
-int resultado;
-int vel = 128;                                //Velocidade de 0 à 255
+int vel = 128;                            //Velocidade de 0 à 255
 
 void setup() {
   Serial.begin(115200);
   pinMode(botao, INPUT);
   pinMode(led_verde, OUTPUT);
   pinMode(led_vermelho, OUTPUT);
-  pinMode(motor_1A, OUTPUT);
-  pinMode(motor_1B, OUTPUT);
-  pinMode(motor_2A, OUTPUT);
-  pinMode(motor_2B, OUTPUT);
+  pinMode(motor1control, OUTPUT);
+  pinMode(motor1A, OUTPUT);
+  pinMode(motor1B, OUTPUT);
+  pinMode(motor2control, OUTPUT);
+  pinMode(motor2A, OUTPUT);
+  pinMode(motor2B, OUTPUT);
 }
 
 void loop() {
   sensor = digitalRead(botao);
   Serial.print("Estado do sensor = ");
   Serial.println(sensor);
-  if (sensor == LOW) {                        //Se o sensor não captar obstáculos, acender LED verde e seguir em frente
-    delay(10000); 		              //Tempo de partida de 10 segundos
+  if (sensor == LOW) {                    //Se o sensor não captar obstáculos, acender LED verde e seguir em frente
+    delay(10000);                         //Tempo de partida de 10 segundos
     digitalWrite(led_verde, HIGH);
     digitalWrite(led_vermelho, LOW);
     Serial.println("Indo para frente");
     tempo_inicial = millis();
-    analogWrite(motor_1A, 0);                 //Motor direito seguir em frente
-    analogWrite(motor_1B, vel);
-    analogWrite(motor_2A, 0);                 //Motor esquerdo seguir em frente
-    analogWrite(motor_2B, vel);
+    analogWrite(motor1control, vel);      //Motor esquerdo seguir em frente
+    digitalWrite(motor1A, HIGH);
+    digitalWrite(motor1B, LOW);
+    analogWrite(motor2control, vel);      //Motor direito seguir em frente
+    digitalWrite(motor2A, HIGH);
+    digitalWrite(motor2B, LOW);
   }
-  else if (sensor == HIGH) {                  //Se o sensor captar obstáculos, acender LED vermelho e dar ré até dar o tempo de duração do percurso ocorrido
+  else if (sensor == HIGH) {              //Se o sensor captar obstáculos, acender LED vermelho e dar ré até dar o tempo de duração do percurso ocorrido
     tempo_parcial = tempo_inicial;
     tempo_final = millis();
     digitalWrite(led_verde, LOW);
     digitalWrite(led_vermelho, HIGH);
     Serial.println("Indo para trás");
-    analogWrite(motor_1A, vel);               //Motor direito dar ré
-    analogWrite(motor_1B, 0);
-    analogWrite(motor_2A, vel);               //Motor esquerdo dar ré
-    analogWrite(motor_2B, 0);
+    analogWrite(motor1control, vel);      //Motor esquerdo dar ré
+    digitalWrite(motor1A, LOW);
+    digitalWrite(motor1B, HIGH);
+    analogWrite(motor2control, vel);      //Motor direito dar ré
+    digitalWrite(motor2A, LOW);
+    digitalWrite(motor2B, HIGH);
     if (tempo_final == tempo_parcial) {
-      analogWrite(motor_1A, 0);               //Motor direito parar
-      analogWrite(motor_1B, 0);
-      analogWrite(motor_2A, 0);               //Motor esquerdo parar
-      analogWrite(motor_2B, 0);
+      analogWrite(motor1control, 0);      //Motor esquerdo parar
+      digitalWrite(motor1A, LOW);
+      digitalWrite(motor1B, LOW);
+      analogWrite(motor2control, 0);      //Motor direito parar
+      digitalWrite(motor2A, LOW);
+      digitalWrite(motor2B, LOW);
     }
   }
 }
